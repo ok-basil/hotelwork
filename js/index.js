206e35fc624d4216d8adb82cb377a0a858fc7b3d
Basil Okache <basilokache@gmail.com>
2023-02-05 16:29:08 +0100

    nav main and top mobile have been done

--- js/index.js
+++ js/index.js
@@ -16,35 +16,36 @@ document.addEventListener("DOMContentLoaded", function () {
         // document.getElementById("nav-bg").classList.remove("nav-bg");
 
         //Apply styles to the logo-search 
-        document.getElementById("logo-search").classList.add("logo-search-normal");
+        document.getElementById("logo-search").classList.add("logo-search-sm");
         document.getElementById("logo-search").classList.remove("logo-search");
 
-        //Apply styles to the best tour plan
-        document.getElementById("best-tour-plan").classList.add("best-tour-plan-normal");
-        document.getElementById("best-tour-plan").classList.remove("best-tour-plan");
-
         //Apply this style to my nav search input
-        document.getElementById("nav-search-input").classList.add("search-input-normal");
+        document.getElementById("nav-search-input").classList.add("search-input-sm");
         document.getElementById("nav-search-input").classList.remove("search-input");
 
+        //Apply styles to the best tour plan
+        document.getElementById("best-tour-plan").classList.add("best-tour-plan-sm");
+        document.getElementById("best-tour-plan").classList.remove("best-tour-plan");
 
+        document.getElementById("menu").classList.add("menu-sm");
+        document.getElementById("menu").classList.remove("menu");
 
-    } else if (viewportWidth >= 768 && viewportWidth < 992) {
-        // Apply styles for medium screens
+    // } else if (viewportWidth >= 768 && viewportWidth < 992) {
+    //     // Apply styles for medium screens
 
-        //Apply this style to my nav-menu
-        document.getElementById("dropdown").classList.remove("");
-        //Apply this style to my nav-bg
-        document.getElementById("nav-bg").classList.remove("nav-bg-normal");
+    //     //Apply this style to my nav-menu
+    //     document.getElementById("dropdown").classList.remove("");
+    //     //Apply this style to my nav-bg
+    //     document.getElementById("nav-bg").classList.remove("nav-bg-normal");
 
-        //Apply this style to my logo-search
-        document.getElementById("logo-search").classList.remove("logo-search-normal");
+    //     //Apply this style to my logo-search
+    //     document.getElementById("logo-search").classList.remove("logo-search-normal");
 
-        //Apply this style to my best-tour-plan
-        document.getElementById("best-tour-plan").classList.remove("best-tour-plan-normal");
+    //     //Apply this style to my best-tour-plan
+    //     document.getElementById("best-tour-plan").classList.remove("best-tour-plan-normal");
 
-        //Apply this style to my nav search input
-        document.getElementById("nav-search-input").classList.remove("search-input-normal");
+    //     //Apply this style to my nav search input
+    //     document.getElementById("nav-search-input").classList.remove("search-input-normal");
 
     } else {
         // Apply styles for large screens
